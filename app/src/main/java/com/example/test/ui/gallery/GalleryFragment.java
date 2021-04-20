e168a08dd6adf73ded20b71bc3d88fa10ecb564e
Jonelle Gamble <jonellegamble@gmail.com>
2021-04-20 17:33:18 -0500

    Adds add/sub Water intake, input number calories, still many fixes to be done

--- app/src/main/java/com/example/test/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/test/ui/gallery/GalleryFragment.java
@@ -1,6 +1,7 @@
 package com.example.test.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.test.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    int num=0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,23 +30,58 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
-        Button btn=(Button)root.findViewById(R.id.nutrients);
-        btn.setOnClickListener(new View.OnClickListener(){
+        Button btnN=(Button)root.findViewById(R.id.nutrients);
+        btnN.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                FragmentTransaction fr=getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment, new Nutrients());
-                fr.commit();
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.placeholder, new Nutrients());
+                fragmentTransaction.commit();
+
+            }
+        });
+        FloatingActionButton btnP=(FloatingActionButton) root.findViewById(R.id.floatingActionButton1);
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView textView = root.findViewById(R.id.numWater);
+                galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                    @Override
+                    public void onChanged( String n) {
+                        textView.setText(addOne(n)+" glasses");
+                    }
+                });
+            }
+        });
+        FloatingActionButton btnM=(FloatingActionButton) root.findViewById(R.id.floatingActionButton2);
+        btnM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView textView = root.findViewById(R.id.numWater);
+                galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                    @Override
+                    public void onChanged( String n) {
+                        textView.setText(subOne(n)+" glasses");
+                    }
+                });
             }
         });
         return root;
+    }
+
+    private String addOne(String n) {
+        num=num+1;
+        n=String.valueOf(num);
+        return (n);
+    }
+    private String subOne(String n) {
+        if(num>0)
+        {
+            num=num-1;
+        }
+        n=String.valueOf(num);
+        return (n);
     }
 }
--- app/src/main/java/com/example/test/ui/gallery/Nutrients.java
+++ app/src/main/java/com/example/test/ui/gallery/Nutrients.java
@@ -2,13 +2,19 @@ package com.example.test.ui.gallery;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.test.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class Nutrients extends Fragment {
@@ -17,6 +23,18 @@ public class Nutrients extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutrients, container, false);
+        View root= inflater.inflate(R.layout.fragment_nutrients, container, false);
+
+        FloatingActionButton backbtn=(FloatingActionButton) root.findViewById(R.id.back);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.content, new GalleryFragment());
+                fragmentTransaction.commit();
+            }
+        });
+        return root;
     }
 }
